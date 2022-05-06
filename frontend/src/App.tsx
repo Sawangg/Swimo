8e237c7b28b65d14d4728bba6792e57212b82765
cesarombredane <combredane@pulso.local>
2022-05-06 14:07:49 +0200

    init front setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import House from "./views/House";
+import Offer from "./views/Offer";
+import Profile from "./views/Profile";
+import Visit from "./views/Visit";
+
 export default function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/House" element={<House />} />
+                <Route path="/Offer" element={<Offer />} />
+                <Route path="/Profile" element={<Profile />} />
+                <Route path="/Visit" element={<Visit />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
---
+++ frontend/src/views/Profile.tsx
@@ -0,0 +1,7 @@
+export default function Pofile() {
+  return (
+    <div>
+      Pofile
+    </div>
+  );
+}
